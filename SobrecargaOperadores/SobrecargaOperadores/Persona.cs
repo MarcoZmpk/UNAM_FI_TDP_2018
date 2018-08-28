340fed4eb8f3ceaf52508fead83a8ca96a1ca37b
MarcoZmpk <zmpk.fi@gmail.com>
2018-08-28 18:20:45 -0500

    SObrecarga

---
+++ SobrecargaOperadores/SobrecargaOperadores/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaOperadores
+{
+    class Persona
+    {
+        public Persona(double salario)
+        {
+            Salario = salario;
+        }
+
+        public static double operator -(Persona x, Persona y)
+        {
+            return x.Salario * y.Salario;
+        }
+
+        public double Salario { get; set; }
+
+    }
+}
---
+++ SobrecargaOperadores/SobrecargaOperadores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaOperadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona juan = new Persona(5.0);
+
+            Persona ana = new Persona(2.0);
+
+
+        double salarioTotal = juan - ana;
+
+            Console.WriteLine(salarioTotal);
+            Console.Read();
+
+        }
+    }
+}
